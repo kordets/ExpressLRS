6d688807abb719d4da7070fc4116bd20dfcfc046
cruwaller <cruwaller@gmail.com>
2020-08-04 14:43:32 +0300

    python: serial handling harmonized and moved new file

--- src/python/BFinitPassthrough.py
+++ src/python/BFinitPassthrough.py
@@ -1,54 +1,17 @@
 import serial
 from xmodem import XMODEM
 import time
-import sys, glob
-
-result = []
-
+import sys
+import serials_find
 
 try:
     requestedBaudrate = int(sys.argv[1])
 except:
     requestedBaudrate = 420000
 
-def serial_ports():
-    """ Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-
-print(serial_ports())
-sys.stdout.write("Dected the following serial ports on this system: \n")
-sys.stdout.write(str(serial_ports())[1:-1])
-sys.stdout.write("\n")
-
-if(len(result) == 0):
-    raise EnvironmentError('No valid serial port detected or port already open')
-
-print("Going to try using "+ result[0])
-
-s = serial.Serial(port=result[0], baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=5, xonxoff=0, rtscts=0)
+port = serials_find.get_serial_port()
+print("Going to try using %s" % port)
+s = serial.Serial(port=port, baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=5, xonxoff=0, rtscts=0)
 
 s.write(chr(0x23).encode())
 time.sleep(1)
@@ -62,6 +25,7 @@ SerialInfoSplit = []
 
 SerialRXindex = -1
 uartNumber = -1
+
 print()
 print()
 print("Attempting to detect Betaflight UART configuration...")
@@ -73,7 +37,6 @@ while s.in_waiting:
         # just discard possible RC / TLM data
         pass
 
-
 for line in inChars.split('\n'):
     if "serial" in line:
         lines.append(line)
@@ -81,7 +44,6 @@ for line in inChars.split('\n'):
         sys.stdout.write("\n")
 
 sys.stdout.flush()
-
 
 for line in lines:
     if line[0:6] == "serial":
@@ -112,7 +74,6 @@ sys.stdout.write("Setting serial passthrough...\n")
 s.write(("serialpassthrough "+str(SerialInfoSplit[SerialRXindex][1])+" "+str(requestedBaudrate)+'\n').encode())
 sys.stdout.write(("serialpassthrough "+str(SerialInfoSplit[SerialRXindex][1])+" "+str(requestedBaudrate)+'\n'))
 time.sleep(1)
-
 
 sys.stdout.flush()
 s.flush()
--- src/python/UARTupload.py
+++ src/python/UARTupload.py
@@ -3,11 +3,10 @@ from xmodem import XMODEM
 import time
 import sys
 import logging
-import os, glob
+import os
+import serials_find
 
 logging.basicConfig(level=logging.ERROR)
-
-result = []
 
 filename = ''
 filesize = 0
@@ -17,6 +16,10 @@ BootloaderInitSeq1 = bytes([0xEC,0x04,0x32,0x62,0x6c,0x0A])
 BootloaderInitSeq2 = bytes([0x62,0x62,0x62,0x62,0x62,0x62])
 
 sys.stdout.flush()
+
+def dbg_print(line):
+    sys.stdout.write(line)
+    sys.stdout.flush()
 
 try:
     filename = sys.argv[1]
@@ -29,53 +32,11 @@ if not os.path.exists(filename):
     dbg_print(msg)
     raise EnvironmentError(msg)
 
-def dbg_print(line):
-    sys.stdout.write(line)
-    sys.stdout.flush()
-
-def serial_ports():
-    """ Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-
-serial_ports()
-
-#sys.stdout.write("Dected the following serial ports on this system: \n")
-#sys.stdout.write(str(serial_ports())[1:-1])
-#sys.stdout.write("\n")
-
-if not result:
-    msg = "\n[FAILED] Cannot find suitable serial port\n\n"
-    dbg_print(msg)
-    raise EnvironmentError(msg)
-
-dbg_print("Going to use "+ result[0]+"\n")
-
-s = serial.Serial(port=result[0], baudrate=420000, bytesize=8, parity='N', stopbits=1, timeout=.5, xonxoff=0, rtscts=0)
-
+port = serials_find.get_serial_port()
+dbg_print("Going to use %s\n" % port)
+s = serial.Serial(port=port, baudrate=420000, bytesize=8, parity='N', stopbits=1, timeout=.5, xonxoff=0, rtscts=0)
 s.timeout = 1.
+
 try:
     already_in_bl = s.read(3).decode('utf-8')
 except UnicodeDecodeError:
