e33bd309e77c6f94d3bd9427e12164a2b639177a
cruwaller <cruwaller@gmail.com>
2020-08-06 00:16:40 +0300

    rx_servo: write output in us to avoid unnecessary conversions

--- src/src/rx/rx_servo_out.cpp
+++ src/src/rx/rx_servo_out.cpp
@@ -51,16 +51,20 @@ void ICACHE_RAM_ATTR servo_out_write(volatile crsf_channels_t &channels) {
     uint32_t now = millis();
     if (servo_update_interval < (now - last_update)) {
 #if (SERVO_PIN_CH1 != UNDEF_PIN)
-        ch1.write(map(channels.ch0, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 0, 180));
+        //ch1.write(map(channels.ch0, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 0, 180));
+        ch1.writeMicroseconds(map(channels.ch0, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 1000, 2000));
 #endif
 #if (SERVO_PIN_CH2 != UNDEF_PIN)
-        ch2.write(map(channels.ch1, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 0, 180));
+        //ch2.write(map(channels.ch1, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 0, 180));
+        ch2.writeMicroseconds(map(channels.ch1, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 1000, 2000));
 #endif
 #if (SERVO_PIN_CH3 != UNDEF_PIN)
-        ch3.write(map(channels.ch2, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 0, 180));
+        //ch3.write(map(channels.ch2, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 0, 180));
+        ch3.writeMicroseconds(map(channels.ch2, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 1000, 2000));
 #endif
 #if (SERVO_PIN_CH4 != UNDEF_PIN)
-        ch4.write(map(channels.ch3, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 0, 180));
+        //ch4.write(map(channels.ch3, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 0, 180));
+        ch4.writeMicroseconds(map(channels.ch3, CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 1000, 2000));
 #endif
         last_update = now;
     }
