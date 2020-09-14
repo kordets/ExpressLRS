43691b251ea3337d2bc048186d2458d9cc9b0bc2
cruwaller <cruwaller@gmail.com>
2020-09-14 11:02:04 +0300

    helpers: contrain added to mappings

--- src/lib/utils/helpers.h
+++ src/lib/utils/helpers.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #define PACKED __attribute__((packed))
+#define CONSTRAIN(amt, low, high) ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)))
 
 // Macros for big-endian (assume little endian host for now) etc
 #define BYTE_SWAP_U16(x) ((uint16_t)__builtin_bswap16(x))
@@ -20,19 +21,19 @@ static inline long MAP(long x, long in_min, long in_max,
     {
         return -1; //AVR returns -1, SAM returns 0
     }
-    return (x - in_min) * (out_max - out_min) / divisor + out_min;
+    return CONSTRAIN((x - in_min) * (out_max - out_min) / divisor + out_min, out_min, out_max);
 }
 
 static inline float MAP_F(float x, float in_min, float in_max,
                           float out_min, float out_max)
 {
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    return CONSTRAIN((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min, out_min, out_max);
 }
 
 static inline uint16_t MAP_U16(uint16_t x, uint16_t in_min, uint16_t in_max,
                                uint16_t out_min, uint16_t out_max)
 {
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    return CONSTRAIN((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min, out_min, out_max);
 };
 
 #endif /* HELPERS_H_ */
