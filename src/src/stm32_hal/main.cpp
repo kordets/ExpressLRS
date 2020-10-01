97f8279558f3b81c7c9f7a906678630af1c618e1
cruwaller <cruwaller@gmail.com>
2020-10-02 00:15:53 +0300

    STM32 HAL: stm32f1x fixes

--- src/src/stm32_hal/main.cpp
+++ src/src/stm32_hal/main.cpp
@@ -151,27 +151,32 @@ uint32_t millis(void)
 {
     uint32_t ms = read_u32(&_ms_cntr);
     return ms;
-    //return HAL_GetTick();
 }
 
 void delay(uint32_t ms)
 {
-    //HAL_Delay(ms);
-    ms += millis();
-    while (timer_is_before(millis(), ms))
-        ;
+    uint32_t start = millis();
+    //ms += millis();
+    //while (timer_is_before(millis(), ms))
+    //    ;
+    while((millis() - start) < ms);
 }
 
 void delayMicroseconds(uint32_t usecs)
 {
+    #if 0
     if (!(CoreDebug->DEMCR & CoreDebug_DEMCR_TRCENA_Msk)) {
         CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
         DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
     }
+    #endif
 
-    uint32_t end = timer_read_time() + microsecondsToClockCycles(usecs);
-    while (timer_is_before(timer_read_time(), end))
-        ;
+    //uint32_t end = timer_read_time() + microsecondsToClockCycles(usecs);
+    //while (timer_is_before(timer_read_time(), end))
+    //    ;
+    usecs = microsecondsToClockCycles(usecs);
+    uint32_t start = timer_read_time();
+    while ((timer_read_time() - start) < usecs);
 }
 
 void HAL_IncTick(void)
@@ -226,20 +231,29 @@ static void timer_init(void)
   */
 static void SystemClock_Config(void)
 {
+    uint32_t FLatency = FLASH_LATENCY_2; // delay = (CPU CLK / 24MHz)
     RCC_OscInitTypeDef RCC_OscInitStruct;
     RCC_ClkInitTypeDef RCC_ClkInitStruct;
-
-#if !USE_INTERNAL_XO
     RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
     /* Initializes the CPU, AHB and APB busses clocks */
+#if !USE_INTERNAL_XO
+    //
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
     RCC_OscInitStruct.HSEState = RCC_HSE_ON;
     RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
     RCC_OscInitStruct.HSIState = RCC_HSI_ON;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9; // 8MHz * 9 = 72MHz
+#else // USE_INTERNAL_XO
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI; // HSI = 8MHz
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2; // 8MHz / 2 = 4MHz
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16; // 4MHz * 16 = 64MHz
+#endif // !USE_INTERNAL_XO
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
         Error_Handler();
     }
@@ -251,8 +265,7 @@ static void SystemClock_Config(void)
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLatency) != HAL_OK) {
         Error_Handler();
     }
 
@@ -262,32 +275,6 @@ static void SystemClock_Config(void)
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
         Error_Handler();
     }
-
-#else // USE_INTERNAL_XO
-    /** Initializes the CPU, AHB and APB busses clocks
-     */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-        Error_Handler();
-    }
-    /** Initializes the CPU, AHB and APB busses clocks
-     */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-                                    RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
-        Error_Handler();
-    }
-#endif // !USE_INTERNAL_XO
 
     SystemCoreClockUpdate();
 }
@@ -343,8 +330,8 @@ static uint32_t dwt_init(void)
 
     /* 3 NO OPERATION instructions */
     __asm volatile(" nop      \n\t"
-                    " nop      \n\t"
-                    " nop      \n\t");
+                   " nop      \n\t"
+                   " nop      \n\t");
 
     /* Check if clock cycle counter has started */
     return (DWT->CYCCNT) ? 0 : 1;
@@ -362,9 +349,6 @@ static void init(void)
         Error_Handler();
     }
 #endif
-
-    /* Initialize the HAL */
-    //HAL_Init();
 
     /* Configure the system clock */
     SystemClock_Config();
@@ -432,8 +416,58 @@ int main(void)
     return 0;
 }
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void SysTick_Handler(void)
 {
-    //HAL_IncTick();
     ++_ms_cntr;
 }
+
+void NMI_Handler(void) {}
+void HardFault_Handler(void) {Error_Handler();}
+void MemManage_Handler(void) {Error_Handler();}
+void BusFault_Handler(void) {Error_Handler();}
+void UsageFault_Handler(void) {Error_Handler();}
+void SVC_Handler(void) {}
+void DebugMon_Handler(void) {}
+void PendSV_Handler(void) {}
+// SysTick_Handler
+void WWDG_IRQHandler(void) {Error_Handler();}
+void PVD_IRQHandler(void) {Error_Handler();}
+void TAMPER_IRQHandler(void) {Error_Handler();}
+void RTC_IRQHandler(void) {Error_Handler();}
+void FLASH_IRQHandler(void) {Error_Handler();}
+void RCC_IRQHandler(void) {Error_Handler();}
+void DMA1_Channel1_IRQHandler(void) {Error_Handler();}
+//void DMA1_Channel2_IRQHandler(void) {Error_Handler();}
+void DMA1_Channel3_IRQHandler(void) {Error_Handler();}
+//void DMA1_Channel4_IRQHandler(void) {Error_Handler();}
+void DMA1_Channel5_IRQHandler(void) {Error_Handler();}
+//void DMA1_Channel6_IRQHandler(void) {Error_Handler();}
+void DMA1_Channel7_IRQHandler(void) {Error_Handler();}
+void ADC1_2_IRQHandler(void) {Error_Handler();}
+void USB_HP_CAN1_TX_IRQHandler(void) {Error_Handler();}
+void USB_LP_CAN1_RX0_IRQHandler(void) {Error_Handler();}
+void CAN1_RX1_IRQHandler(void) {Error_Handler();}
+void CAN1_SCE_IRQHandler(void) {Error_Handler();}
+void TIM1_BRK_IRQHandler(void) {Error_Handler();}
+void TIM1_UP_IRQHandler(void) {Error_Handler();}
+void TIM1_TRG_COM_IRQHandler(void) {Error_Handler();}
+void TIM1_CC_IRQHandler(void) {Error_Handler();}
+//void TIM2_IRQHandler(void) {Error_Handler();}
+void TIM3_IRQHandler(void) {Error_Handler();}
+void TIM4_IRQHandler(void) {Error_Handler();}
+void I2C1_EV_IRQHandler(void) {Error_Handler();}
+void I2C1_ER_IRQHandler(void) {Error_Handler();}
+void I2C2_EV_IRQHandler(void) {Error_Handler();}
+void I2C2_ER_IRQHandler(void) {Error_Handler();}
+void SPI1_IRQHandler(void) {Error_Handler();}
+void SPI2_IRQHandler(void) {Error_Handler();}
+void RTC_Alarm_IRQHandler(void) {Error_Handler();}
+void USBWakeUp_IRQHandler(void) {Error_Handler();}
+
+#ifdef __cplusplus
+}
+#endif
