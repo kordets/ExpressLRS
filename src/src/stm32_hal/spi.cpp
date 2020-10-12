9e4dea2a2bcb91c021809eae360be6277bf346e0
cruwaller <cruwaller@gmail.com>
2020-10-12 22:11:44 +0300

    stm32 hal: L4xx SPI fix

--- src/src/stm32_hal/spi.cpp
+++ src/src/stm32_hal/spi.cpp
@@ -1,10 +1,8 @@
 #include "internal.h"
 #include "helpers.h"
 #include "gpio.h"
-
-#define SPI_CR1_CPHA_Pos (0U)
-
-#define SPI_CR1_BR_Pos (3U)
+#include "stm32_def.h"
+#include "debug_elrs.h"
 
 struct spi_info
 {
@@ -31,6 +29,8 @@ static const struct spi_info spi_bus[] = {
 #endif
 };
 
+
+#define USE_HAL_VERSION 0
 
 struct spi_config
 spi_setup(uint32_t speed, int sck, int miso, int mosi, uint8_t mode)
@@ -59,12 +59,51 @@ spi_setup(uint32_t speed, int sck, int miso, int mosi, uint8_t mode)
         uint32_t div = 0;
         while ((pclk >> (div + 1)) > speed && div < 7)
             div++;
+#if !USE_HAL_VERSION
         uint32_t cr1 = ((mode << SPI_CR1_CPHA_Pos) |
                         (div << SPI_CR1_BR_Pos) |
                         SPI_CR1_SPE | SPI_CR1_MSTR |
                         SPI_CR1_SSM | SPI_CR1_SSI);
-
+#ifdef STM32L4xx
+        spi->CR2 = SPI_DATASIZE_8BIT | SPI_RXFIFO_THRESHOLD_QF;
+#endif
         return (struct spi_config){.spi = spi, .spi_cr1 = cr1};
+#else
+        static SPI_HandleTypeDef handle;
+        handle.Instance = spi;
+        handle.Init.BaudRatePrescaler = (div << SPI_CR1_BR_Pos);
+        handle.Init.Direction = SPI_DIRECTION_2LINES;
+        switch (mode) {
+            case 0:
+                handle.Init.CLKPhase = SPI_PHASE_1EDGE;
+                handle.Init.CLKPolarity = SPI_POLARITY_LOW;
+                break;
+            case 1:
+                handle.Init.CLKPhase = SPI_PHASE_2EDGE;
+                handle.Init.CLKPolarity = SPI_POLARITY_LOW;
+                break;
+            case 2:
+                handle.Init.CLKPhase = SPI_PHASE_1EDGE;
+                handle.Init.CLKPolarity = SPI_POLARITY_HIGH;
+                break;
+            case 3:
+                handle.Init.CLKPhase = SPI_PHASE_2EDGE;
+                handle.Init.CLKPolarity = SPI_POLARITY_HIGH;
+                break;
+        }
+        handle.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLED;
+        handle.Init.DataSize = SPI_DATASIZE_8BIT;
+        handle.Init.FirstBit = SPI_FIRSTBIT_MSB;
+        handle.Init.NSS = SPI_NSS_SOFT;
+        handle.Init.TIMode = SPI_TIMODE_DISABLED;
+        handle.Init.Mode = SPI_MODE_MASTER;
+        if (HAL_SPI_Init(&handle) != HAL_OK) {
+            DEBUG_PRINTF("SPI Init failed!\n");
+            Error_Handler();
+        }
+        __HAL_SPI_ENABLE(&handle);
+        return (struct spi_config){.spi = &handle, .spi_cr1 = 0};
+#endif
     }
     return {.spi = NULL, .spi_cr1 = 0};
 }
@@ -73,18 +112,25 @@ void spi_prepare(struct spi_config config)
 {
     SPI_TypeDef *spi = (SPI_TypeDef *)config.spi;
     if (!spi) return;
+#if !USE_HAL_VERSION
     spi->CR1 = config.spi_cr1;
+#endif
 }
 
 void spi_transfer(struct spi_config config, uint8_t receive_data, uint8_t len, uint8_t *data)
 {
+#if !USE_HAL_VERSION
     SPI_TypeDef *spi = (SPI_TypeDef *)config.spi;
+#else
+    SPI_HandleTypeDef * handle = (SPI_HandleTypeDef*)config.spi;
+    SPI_TypeDef *spi = (SPI_TypeDef *)handle->Instance;
+#endif
     if (!spi) return;
     while (len--) {
-        spi->DR = *data;
         //while (!(spi->SR & (SPI_SR_TXE)));
+        *(__IO uint8_t *)&spi->DR = *data;
         while (!(spi->SR & SPI_SR_RXNE));
-        uint8_t rdata = spi->DR;
+        uint8_t rdata = *(__IO uint8_t *)&spi->DR;
         if (receive_data)
             *data = rdata;
         data++;
