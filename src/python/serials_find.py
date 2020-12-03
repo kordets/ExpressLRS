58f1f6ce85c6f1264ddecb7a1602468741c246b9
cruwaller <cruwaller@gmail.com>
2020-12-03 23:31:40 +0200

    bootloader updates + env cleanups

--- src/python/serials_find.py
+++ src/python/serials_find.py
@@ -32,6 +32,7 @@ def serial_ports():
         platform = sys.platform.lower()
         if platform.startswith('win'):
             ports = ['COM%s' % (i + 1) for i in range(256)]
+            ports.reverse()
         elif platform.startswith('linux') or platform.startswith('cygwin'):
             # this excludes your current terminal "/dev/tty"
             #ports = glob.glob('/dev/tty[A-Za-z]*')
