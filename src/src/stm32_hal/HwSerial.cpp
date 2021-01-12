46cfbbf05b22446e72aafb39b7135811d7e7769c
cruwaller <cruwaller@gmail.com>
2021-01-12 21:11:12 +0200

    HwSerial: code cleanup

--- src/src/stm32_hal/HwSerial.cpp
+++ src/src/stm32_hal/HwSerial.cpp
@@ -3,24 +3,8 @@
 #include "Arduino.h"
 #include "gpio.h"
 
-#if defined(GPIO_PIN_RCSIGNAL_RX) && defined(GPIO_PIN_RCSIGNAL_TX)
-#define SPORT_RX_TX GPIO_PIN_RCSIGNAL_RX, GPIO_PIN_RCSIGNAL_TX
-#else
-#if (SPORT == PB10)
-// USART3
-#define SPORT_RX_TX PB11, SPORT
-#elif (SPORT == PA2)
-// USART2
-#define SPORT_RX_TX PA3, SPORT
-#elif (SPORT == PA9)
-// USART1
-#define SPORT_RX_TX PA10, SPORT
-#else
-#error "No valid S.Port UART TX pin defined!"
-#endif
-#endif
 
-HwSerial CrsfSerial(SPORT_RX_TX, BUFFER_OE);
+HwSerial CrsfSerial(GPIO_PIN_RCSIGNAL_RX, GPIO_PIN_RCSIGNAL_TX, BUFFER_OE);
 
 HwSerial::HwSerial(uint32_t _rx, uint32_t _tx, int32_t pin)
     : HardwareSerial(_rx, _tx, 1)
