2f6f413e9816a8473680c8b38c5d64bd2798258f
cruwaller <cruwaller@gmail.com>
2021-04-02 00:31:26 +0300

    OTA CRC updated to match koopman's

--- src/lib/utils/crc.h
+++ src/lib/utils/crc.h
@@ -4,12 +4,11 @@
 #include "platform.h"
 #include <stdint.h>
 
-#define CRC16_POLY_NEW 15
 
 uint8_t CalcCRC(uint8_t data, uint8_t crc);
 uint8_t CalcCRC(volatile uint8_t const *data, uint16_t length, uint8_t crc = 0);
 uint8_t CalcCRC(uint8_t const *data, uint16_t length, uint8_t crc = 0);
-uint8_t CalcCRCxor(uint8_t *data, uint16_t length, uint8_t crc = 0);
+uint8_t CalcCRCxor(uint8_t const *data, uint16_t length, uint8_t crc = 0);
 uint8_t CalcCRCxor(uint8_t data, uint8_t crc = 0);
 uint16_t CalcCRC16(uint8_t const *data, uint16_t length, uint16_t crc = 0);
 
@@ -17,10 +16,6 @@ uint8_t crc8_dvb_s2(uint8_t crc, uint8_t a);
 uint8_t crc8_dvb_s2(uint8_t const* data, uint16_t length);
 
 uint32_t CalcCRC32(uint8_t const *data, uint16_t len);
-
-#if (CRSF_CMD_CRC)
-uint8_t CalcCRCcmd(uint8_t const *data, uint16_t length, uint8_t crc = 0);
-#endif
 
 #if CRC16_POLY_TESTING
 extern uint8_t CRC16_POLY_PKT[5];
